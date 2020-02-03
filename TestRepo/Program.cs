9ceb20e1a75302c76aba168d880e1ad96d97e913
Omid Chini Foroushan <o.foroushan@tricentis.com>
2020-02-03 12:39:19 +0100

    A important bug fix

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace TestRe_p {
     class Program {
         static void Main(string[] args) {
+            //a very urgent bug fix by someone else
         }
     }
 }
