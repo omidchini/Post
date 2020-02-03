0c9db040cf0dd87b5f5d4b8a26e9302f8ec8d7c3
Omid Chini Foroushan <o.foroushan@tricentis.com>
2020-02-03 12:28:26 +0100

    1st feature, 2nd feature, 3rd feature

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace TestRe_p {
     class Program {
         static void Main(string[] args) {
+            //first feature
+            //a bug fix
+            //another feature
             //a very urgent bug fix by someone else
         }
     }
