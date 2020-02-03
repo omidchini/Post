99da856cff6c7f5c9be8f8ffb08da4228f1cde88
Omid Chini Foroushan <o.foroushan@tricentis.com>
2020-02-03 12:28:39 +0100

    Refactoring

---
+++ TestRepo/MainProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace TestRe_p {
+    internal class MainProgram {
+        private static void Main(string[] args) {
+            //first feature
+            //a bug fix
+            //another feature
+            //a very urgent bug fix by someone else
+        }
+    }
+}
